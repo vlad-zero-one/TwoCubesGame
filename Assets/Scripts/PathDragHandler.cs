5fc0a5055b80745afb98e21b4bc25808c9612194
Vladislav Igorevich <vladislav.igorevich.workmail@gmail.com>
2022-11-05 22:55:09 +0300

    DragHandler, Spawner, Arrows prefab

---
+++ Assets/Scripts/PathDragHandler.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class PathDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Vector3 mousePosition;
+        
+    private bool mousePressed;
+
+    public NewSegmentEvent newSegmentEvent;
+
+    private void Awake()
+    {
+        if (newSegmentEvent == null)
+        {
+            newSegmentEvent = new NewSegmentEvent();
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        var ray = Camera.main.ScreenPointToRay(eventData.position);
+
+        if (Physics.Raycast(ray, out var hit))
+        {
+            mousePosition = hit.point;
+            mousePressed = true;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        var ray = Camera.main.ScreenPointToRay(eventData.position);
+
+        if (Physics.Raycast(ray, out var hit))
+        {
+            if (Vector3.Distance(mousePosition, hit.point) > 1)
+            {
+                newSegmentEvent?.Invoke(mousePosition, hit.point);
+
+                mousePosition = hit.point;
+            }
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        mousePressed = false;
+    }
+
+    private void OnDestroy()
+    {
+        newSegmentEvent.RemoveAllListeners();
+    }
+
+    public class NewSegmentEvent : UnityEvent<Vector3, Vector3> { }
+}
---
+++ Assets/Scripts/PathElementsSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PathElementsSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject pathElementPrefab;
+    [SerializeField] private PathDragHandler pathDragHandler;
+
+    private void Start()
+    {
+        pathDragHandler.newSegmentEvent.AddListener(SpawnPathElement);
+    }
+
+    private void SpawnPathElement(Vector3 start, Vector3 end)
+    {
+        var middlePosition = (start + end) / 2;
+        middlePosition.y = 0;
+
+        var rotation = Quaternion.LookRotation(end - start);
+        rotation *= Quaternion.Euler(90, 0, 0);
+
+        var elem = Instantiate(pathElementPrefab, middlePosition, rotation);
+        var elemTransform = elem.transform;
+    }
+
+    private void OnDestroy()
+    {
+        pathDragHandler.newSegmentEvent.RemoveListener(SpawnPathElement);
+    }
+}
